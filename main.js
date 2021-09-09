ffe7cc5042d527ac43d383d8fb5cab54138fbb37
lucascoliveira189 <lucascoliveira819@gmail.com>
2021-09-09 18:36:36 -0300

    implemented mathematics class, and functions of main class. Implemented of final version of Simple Calculator

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 window.onload = () => {
     console.log('Window Loaded!!!')
+    const btn = document.getElementById('btnCalculator')
+    btn.onclick = click
+
+    function getInputValue(id) {
+        const item = document.getElementById(id)
+        return item.value
+    }
+
+    function click() {
+        const operationType = getInputValue('operationType')
+        const value1 = getInputValue('value1')
+        const value2 = getInputValue('value2')
+        const result = Mathematics[operationType](
+            value1, value2
+        )
+
+        document.getElementById('result').innerText = `The Operation of ${operationType}, of ${value1} from ${value2} is: ${result}`
+    }
 }
